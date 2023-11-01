32d76e3109f92454ddaffb9fc2a902de157589b8
Thiago.Felix.XD <143654401+ThiagoFelix-xd@users.noreply.github.com>
2023-11-01 12:25:58 -0300

    Add files via upload

---
+++ Vetor.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int vetor[10];
+    int i;
+
+    for (i = 0; i < 10; i++) {
+        printf("Digite o número na posição %d: ", i + 1);
+        scanf("%d", &vetor[i]);
+        vetor[i] = 2 * vetor[i];
+    }
+
+    // Apresentara os resultados //
+    printf("Valores dobrados:\n");
+    for (i = 0; i < 10; i++) {
+        printf("Posição %d: %d\n", i + 1, vetor[i]);
+    }
+
+    return 0;
+}
+
